f6076549d58a941cf728e794c353a4727d570197
leonildomello42 <leonildo.mello@gmail.com>
2021-06-16 21:14:27 -0300

    Finalizando primeira aplicação

---
+++ cap4_primeira_aplicacao/aula26_diretivas/jedi-academy/src/app/student/student.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Student } from './student.model';
+
+@Component({
+  selector: 'jad-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+
+  constructor() { }
+
+  /*
+  @Input() name?: string;
+  @Input() isJedi?: boolean;
+  */
+
+  @Input() student?: Student;
+
+  ngOnInit(): void {
+  }
+
+  clicked(){
+    console.log(`Student: ${this.student?.name}`);
+  }
+}
